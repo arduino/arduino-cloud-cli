e578704ef23dd364be0edb06483ac386f78ad93c
Paolo Calao <paolo.calao@gmail.com>
2021-07-06 16:59:35 +0200

    Wrap iot client go

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/bcmi-labs/iot-cloud-cli
 go 1.15
 
 require (
+	github.com/arduino/iot-client-go v1.3.3
 	github.com/bcmi-labs/oniudra-cli v0.15.8
 	github.com/eclipse/paho.mqtt.golang v1.3.2
 	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
 )
 
 replace github.com/cisco/senml => github.com/bcmi-labs/senml v0.1.0
---
+++ iot/client.go
@@ -0,0 +1,60 @@
+package iot
+
+import (
+	"context"
+	"fmt"
+
+	iotclient "github.com/arduino/iot-client-go"
+)
+
+type Client interface {
+	AddDevice(fqbn, name, serial, devType string) (string, error)
+}
+
+type client struct {
+	ctx context.Context
+	api *iotclient.APIClient
+}
+
+func NewClient(clientID, secretID string) (Client, error) {
+	cl := &client{}
+	err := cl.setup(clientID, secretID)
+	if err != nil {
+		err = fmt.Errorf("instantiate new iot client: %w", err)
+		return nil, err
+	}
+	return cl, nil
+}
+
+func (cl *client) AddDevice(fqbn, name, serial, dType string) (string, error) {
+	payload := iotclient.CreateDevicesV2Payload{
+		Fqbn:   fqbn,
+		Name:   name,
+		Serial: serial,
+		Type:   dType,
+	}
+	dev, _, err := cl.api.DevicesV2Api.DevicesV2Create(cl.ctx, payload)
+	if err != nil {
+		err = fmt.Errorf("creating device, %w", err)
+		return "", err
+	}
+	return dev.Id, nil
+}
+
+func (cl *client) setup(client, secret string) error {
+	// Get the access token in exchange of client_id and client_secret
+	tok, err := token(client, secret)
+	if err != nil {
+		err = fmt.Errorf("cannot retrieve token given client and secret: %w", err)
+		return err
+	}
+
+	// We use the token to create a context that will be passed to any API call
+	cl.ctx = context.WithValue(context.Background(), iotclient.ContextAccessToken, tok.AccessToken)
+
+	// Create an instance of the iot-api Go client, we pass an empty config
+	// because defaults are ok
+	cl.api = iotclient.NewAPIClient(iotclient.NewConfiguration())
+
+	return nil
+}
