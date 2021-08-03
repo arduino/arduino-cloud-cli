9962176c757d640db324815408729a2a2e454a04
Paolo Calao <paolo.calao@gmail.com>
2021-08-03 18:23:56 +0200

    Remove ping

--- cli/root.go
+++ cli/root.go
@@ -6,13 +6,11 @@ import (
 
 	"github.com/arduino/iot-cloud-cli/cli/config"
 	"github.com/arduino/iot-cloud-cli/cli/device"
-	"github.com/arduino/iot-cloud-cli/cli/ping"
 	"github.com/spf13/cobra"
 )
 
 func Execute() {
 	rootCmd := &cobra.Command{}
-	rootCmd.AddCommand(ping.NewCommand())
 	rootCmd.AddCommand(config.NewCommand())
 	rootCmd.AddCommand(device.NewCommand())
 
