dbb096879f4f8892b4097743b0376676bb976250
Paolo Calao <paolo.calao@gmail.com>
2021-08-05 16:21:57 +0200

    Add device delete command

---
+++ command/device/delete.go
@@ -0,0 +1,27 @@
+package device
+
+import (
+	"github.com/arduino/iot-cloud-cli/internal/config"
+	"github.com/arduino/iot-cloud-cli/internal/iot"
+)
+
+// DeleteParams contains the parameters needed to
+// delete a device from Arduino IoT Cloud.
+type DeleteParams struct {
+	ID string
+}
+
+// Delete command is used to delete a device
+// from Arduino IoT Cloud.
+func Delete(params *DeleteParams) error {
+	conf, err := config.Retrieve()
+	if err != nil {
+		return err
+	}
+	iotClient, err := iot.NewClient(conf.Client, conf.Secret)
+	if err != nil {
+		return err
+	}
+
+	return iotClient.DeleteDevice(params.ID)
+}
