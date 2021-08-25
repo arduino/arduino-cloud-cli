28655198a9dcda8e3a0443c8d1f8f9f66d3a3d29
Paolo Calao <paolo.calao@gmail.com>
2021-08-25 17:05:00 +0200

    Add thing bind command

---
+++ cli/thing/bind.go
@@ -0,0 +1,43 @@
+package thing
+
+import (
+	"fmt"
+
+	"github.com/arduino/iot-cloud-cli/command/thing"
+	"github.com/spf13/cobra"
+)
+
+var bindFlags struct {
+	id       string
+	deviceID string
+}
+
+func initBindCommand() *cobra.Command {
+	bindCommand := &cobra.Command{
+		Use:   "bind",
+		Short: "Bind a thing to a device",
+		Long:  "Bind a thing to a device on Arduino IoT Cloud",
+		RunE:  runBindCommand,
+	}
+	bindCommand.Flags().StringVarP(&bindFlags.id, "id", "i", "", "Thing ID")
+	bindCommand.Flags().StringVarP(&bindFlags.deviceID, "device", "d", "", "Device ID")
+	bindCommand.MarkFlagRequired("id")
+	bindCommand.MarkFlagRequired("device")
+	return bindCommand
+}
+
+func runBindCommand(cmd *cobra.Command, args []string) error {
+	fmt.Printf("Binding thing %s to device%s\n", bindFlags.id, bindFlags.deviceID)
+
+	params := &thing.BindParams{
+		ID:       bindFlags.id,
+		DeviceID: bindFlags.deviceID,
+	}
+	err := thing.Bind(params)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Thing-Device bound successfully updated")
+	return nil
+}
---
+++ command/thing/bind.go
@@ -0,0 +1,40 @@
+package thing
+
+import (
+	iotclient "github.com/arduino/iot-client-go"
+	"github.com/arduino/iot-cloud-cli/internal/config"
+	"github.com/arduino/iot-cloud-cli/internal/iot"
+)
+
+// BindParams contains the parameters needed to
+// bind a thing to a device.
+// ID indicates the thing to bind.
+// deviceID indicates the device bind.
+type BindParams struct {
+	ID       string
+	DeviceID string
+}
+
+// Bind command is used to bind a thing to a device
+// on Arduino IoT Cloud.
+func Bind(params *BindParams) error {
+	conf, err := config.Retrieve()
+	if err != nil {
+		return err
+	}
+	iotClient, err := iot.NewClient(conf.Client, conf.Secret)
+	if err != nil {
+		return err
+	}
+
+	thing := &iotclient.Thing{
+		DeviceId: params.DeviceID,
+	}
+
+	err = iotClient.UpdateThing(params.ID, thing, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
