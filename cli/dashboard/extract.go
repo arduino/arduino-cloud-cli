b44033bfcaac6d44fcb2efdcf7b279041029478e
Paolo Calao <p.calao@arduino.cc>
2021-10-12 16:26:31 +0200

    Add dashboard extract command (#47)
    Add a command to extract templates from dashboards:
    '$ arduino-cloud-cli dashboard extract --id <dashboardID> --outfile <templateFile> --format <yaml|json>'
    
    * Add dashboard extract command
    
    * Include dashboard variables only if any var is present
    
    * Add widget options filter
    
    * Include widgets only if not empty
    
    * Update readme

---
+++ cli/dashboard/extract.go
@@ -0,0 +1,74 @@
+// This file is part of arduino-cloud-cli.
+//
+// Copyright (C) 2021 ARDUINO SA (http://www.arduino.cc/)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package dashboard
+
+import (
+	"os"
+
+	"github.com/arduino/arduino-cli/cli/errorcodes"
+	"github.com/arduino/arduino-cli/cli/feedback"
+	"github.com/arduino/arduino-cloud-cli/command/dashboard"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var extractFlags struct {
+	id      string
+	outfile string
+	format  string
+}
+
+func initExtractCommand() *cobra.Command {
+	extractCommand := &cobra.Command{
+		Use:   "extract",
+		Short: "Extract a template from a dashboard",
+		Long:  "Extract a template from a Arduino IoT Cloud dashboard and save it in a file",
+		Run:   runExtractCommand,
+	}
+	extractCommand.Flags().StringVarP(&extractFlags.id, "id", "i", "", "Dashboard ID")
+	extractCommand.Flags().StringVarP(&extractFlags.outfile, "outfile", "o", "", "Template file destination path")
+	extractCommand.Flags().StringVar(
+		&extractFlags.format,
+		"format",
+		"yaml",
+		"Format of template file, can be {json|yaml}. Default is 'yaml'",
+	)
+
+	extractCommand.MarkFlagRequired("id")
+	return extractCommand
+}
+
+func runExtractCommand(cmd *cobra.Command, args []string) {
+	logrus.Infof("Extracting template from dashboard %s\n", extractFlags.id)
+
+	params := &dashboard.ExtractParams{
+		ID:     extractFlags.id,
+		Format: extractFlags.format,
+	}
+	if extractFlags.outfile != "" {
+		params.Outfile = &extractFlags.outfile
+	}
+
+	err := dashboard.Extract(params)
+	if err != nil {
+		feedback.Errorf("Error during template extraction: %v", err)
+		os.Exit(errorcodes.ErrGeneric)
+	}
+
+	logrus.Info("Template successfully extracted")
+}
---
+++ command/dashboard/extract.go
@@ -0,0 +1,81 @@
+// This file is part of arduino-cloud-cli.
+//
+// Copyright (C) 2021 ARDUINO SA (http://www.arduino.cc/)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package dashboard
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/arduino/arduino-cloud-cli/internal/config"
+	"github.com/arduino/arduino-cloud-cli/internal/iot"
+	"github.com/arduino/arduino-cloud-cli/internal/template"
+)
+
+// ExtractParams contains the parameters needed to
+// extract a template dashboard from Arduino IoT Cloud and save it on local storage.
+type ExtractParams struct {
+	ID      string
+	Format  string  // Format determines the file format of the template ("json" or "yaml")
+	Outfile *string // Destination path of the extracted template
+}
+
+// Extract command is used to extract a dashboard template
+// from a dashboard on Arduino IoT Cloud.
+func Extract(params *ExtractParams) error {
+	params.Format = strings.ToLower(params.Format)
+	if params.Format != "json" && params.Format != "yaml" {
+		return errors.New("format is not valid: only 'json' and 'yaml' are supported")
+	}
+
+	conf, err := config.Retrieve()
+	if err != nil {
+		return err
+	}
+	iotClient, err := iot.NewClient(conf.Client, conf.Secret)
+	if err != nil {
+		return err
+	}
+
+	dashboard, err := iotClient.DashboardShow(params.ID)
+	if err != nil {
+		err = fmt.Errorf("%s: %w", "cannot extract dashboard: ", err)
+		return err
+	}
+
+	templ, err := template.FromDashboard(dashboard)
+	if err != nil {
+		return err
+	}
+
+	if params.Outfile == nil {
+		name, ok := templ["name"].(string)
+		if name == "" || !ok {
+			return errors.New("dashboard template does not have a valid name")
+		}
+		outfile := name + "-dashboard" + "." + params.Format
+		params.Outfile = &outfile
+	}
+
+	err = template.ToFile(templ, *params.Outfile, params.Format)
+	if err != nil {
+		return fmt.Errorf("saving template: %w", err)
+	}
+
+	return nil
+}
