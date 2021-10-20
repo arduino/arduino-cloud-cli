462b03bd1e5f96b250157aed9687b6458b5f7b6f
Paolo Calao <paolo.calao@gmail.com>
2021-10-20 13:15:38 +0200

    Fix deferred ota

--- cli/ota/upload.go
+++ cli/ota/upload.go
@@ -56,6 +56,7 @@ func runUploadCommand(cmd *cobra.Command, args []string) {
 	params := &ota.UploadParams{
 		DeviceID: uploadFlags.deviceID,
 		File:     uploadFlags.file,
+		Deferred: uploadFlags.deferred,
 	}
 	err := ota.Upload(params)
 	if err != nil {
