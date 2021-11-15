5136228060986a07e9e3cee6a76b9f7a30691d0b
Paolo Calao <p.calao@arduino.cc>
2021-11-15 19:31:47 +0100

    Keep the happy path on the same level of indentation
    Co-authored-by: Giuseppe Lumia <g.lumia@outlook.com>

--- command/tag/tag.go
+++ command/tag/tag.go
@@ -7,11 +7,11 @@ import "fmt"
 func TagsInfo(tags map[string]interface{}) ([]string, error) {
 	var str []string
 	for key, value := range tags {
-		if valStr, ok := value.(string); ok {
-			str = append(str, key+": "+valStr)
-		} else {
+		valStr, ok := value.(string)
+		if !ok {
 			return nil, fmt.Errorf("value of tag `%s` should be of type `string` but is of type `%T`", key, value)
 		}
+		str = append(str, key+": "+valStr)
 	}
 	return str, nil
 }
