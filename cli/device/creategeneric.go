b1e31f1e61359c4755bd63de775652b00eb88c00
Paolo Calao <paolo.calao@gmail.com>
2021-12-15 11:35:07 +0100

    Specify generic device is less secure

--- cli/device/creategeneric.go
+++ cli/device/creategeneric.go
@@ -36,8 +36,8 @@ var createGenericFlags struct {
 func initCreateGenericCommand() *cobra.Command {
 	createGenericCommand := &cobra.Command{
 		Use:   "create-generic",
-		Short: "Create a generic device",
-		Long:  "Create a generic device for Arduino IoT Cloud",
+		Short: "Create a generic device - without secure element - WARNING: less secure",
+		Long:  "Create a generic device for Arduino IoT Cloud - without secure element - WARNING: less secure",
 		Run:   runCreateGenericCommand,
 	}
 	createGenericCommand.Flags().StringVarP(&createGenericFlags.name, "name", "n", "", "Device name")
