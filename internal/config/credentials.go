d73b98eb761cd7a836f1b3e2aaf6d78b20100f07
Paolo Calao <paolo.calao@gmail.com>
2021-12-22 14:07:26 +0100

    Improve credentials

--- internal/config/credentials.go
+++ internal/config/credentials.go
@@ -38,8 +38,8 @@ const (
 	CredentialsFilename = "arduino-cloud-credentials"
 )
 
-// SetDefaultCredentials sets the default credentials values.
-func SetDefaultCredentials(settings *viper.Viper) {
+// SetEmptyCredentials sets the default credentials values to empty strings.
+func SetEmptyCredentials(settings *viper.Viper) {
 	// Client ID
 	settings.SetDefault("client", "")
 	// Secret
@@ -79,7 +79,8 @@ func (c *Credentials) IsEmpty() bool {
 
 // RetrieveCredentials looks for credentials in
 // environment variables or in credentials file.
-// Returns error if no credentials are found.
+// Returns error if no credentials are found or
+// if found credentials are invalid.
 func RetrieveCredentials() (*Credentials, error) {
 	// Credentials extracted from environment has highest priority
 	logrus.Info("Looking for credentials in environment variables")
@@ -87,49 +88,53 @@ func RetrieveCredentials() (*Credentials, error) {
 	if err != nil {
 		return nil, fmt.Errorf("reading credentials from environment variables: %w", err)
 	}
-	// Return credentials only if found
-	if c != nil {
-		logrus.Info("Credentials found in environment variables")
+	// Return credentials if found in env
+	if !c.IsEmpty() {
+		// Return error if credentials are found but are not valid
+		if err := c.Validate(); err != nil {
+			return nil, fmt.Errorf(
+				"credentials retrieved from environment variables with prefix '%s' are not valid: %w", EnvPrefix, err,
+			)
+		}
+		logrus.Infof("Credentials found in environment variables with prefix '%s'", EnvPrefix)
 		return c, nil
 	}
 
 	logrus.Info("Looking for credentials in file system")
-	c, err = fromFile()
-	if err != nil {
-		return nil, fmt.Errorf("reading credentials from file: %w", err)
-	}
-	if c != nil {
-		return c, nil
-	}
-
-	return nil, fmt.Errorf(
-		"credentials have not been found neither in environment variables " +
-			"nor in the current directory, its parents or in arduino15",
-	)
-}
-
-// fromFile looks for a credentials file.
-// If a credentials file is not found, it returns nil credentials without raising errors.
-// If invalid credentials file is found, it returns an error.
-func fromFile() (*Credentials, error) {
-	// Looks for a credentials file
-	configDir, err := searchConfigDir(CredentialsFilename)
+	filepath, found, err := searchConfigDir(CredentialsFilename)
 	if err != nil {
 		return nil, fmt.Errorf("can't get credentials directory: %w", err)
 	}
-	// Return nil credentials if no config file is found
-	if configDir == nil {
-		return nil, nil
+	if !found {
+		return nil, fmt.Errorf(
+			"credentials have not been found neither in environment variables " +
+				"nor in the current directory, its parents or in arduino15",
+		)
 	}
 
+	c, err = fromFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("reading credentials from file %s: %w", filepath, err)
+	}
+	// Return error if credentials are not valid
+	if err := c.Validate(); err != nil {
+		return nil, fmt.Errorf(
+			"credentials retrieved from file %s are not valid: %w", filepath, err,
+		)
+	}
+	return c, nil
+}
+
+// fromFile looks for a credentials file.
+func fromFile(filepath string) (*Credentials, error) {
 	v := viper.New()
 	v.SetConfigName(CredentialsFilename)
-	v.AddConfigPath(*configDir)
-	err = v.ReadInConfig()
+	v.AddConfigPath(filepath)
+	err := v.ReadInConfig()
 	if err != nil {
 		err = fmt.Errorf(
 			"credentials file found at %s but cannot read its content: %w",
-			*configDir,
+			filepath,
 			err,
 		)
 		return nil, err
@@ -140,26 +145,18 @@ func fromFile() (*Credentials, error) {
 	if err != nil {
 		return nil, fmt.Errorf(
 			"credentials file found at %s but cannot unmarshal it: %w",
-			*configDir,
-			err,
-		)
-	}
-	if err = cred.Validate(); err != nil {
-		return nil, fmt.Errorf(
-			"credentials file found at %s but is not valid: %w",
-			*configDir,
+			filepath,
 			err,
 		)
 	}
 	return cred, nil
 }
 
-// fromEnv looks for credentials in environment variables.
-// If credentials are not found, it returns nil credentials without raising errors.
-// If invalid credentials are found, it returns an error.
+// fromEnv retrieves credentials from environment variables.
+// Returns empty credentials if not found.
 func fromEnv() (*Credentials, error) {
 	v := viper.New()
-	SetDefaultCredentials(v)
+	SetEmptyCredentials(v)
 	v.SetEnvPrefix(EnvPrefix)
 	v.AutomaticEnv()
 
@@ -167,18 +164,6 @@ func fromEnv() (*Credentials, error) {
 	err := v.Unmarshal(cred)
 	if err != nil {
 		return nil, fmt.Errorf("cannot unmarshal credentials from environment variables: %w", err)
-	}
-
-	if cred.IsEmpty() {
-		return nil, nil
-	}
-
-	if err = cred.Validate(); err != nil {
-		return nil, fmt.Errorf(
-			"credentials retrieved from environment variables with prefix '%s' are not valid: %w",
-			EnvPrefix,
-			err,
-		)
 	}
 	return cred, nil
 }
