0881b348ccf7ac49ed9e52659a4e76b1b027393f
Paolo Calao <paolo.calao@gmail.com>
2022-01-04 13:42:36 +0100

    Add test

--- internal/lzss/lzss_test.go
+++ internal/lzss/lzss_test.go
@@ -1,14 +1,50 @@
 package lzss
 
 import (
-	"fmt"
+	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
-func TestContains(t *testing.T) {
-	buf := []byte("ciao a tutti")
-	occ := []byte("tut")
-	fmt.Println(contains(buf, occ))
-	occ = []byte("ti")
-	fmt.Println(contains(buf, occ))
+func TestEncode(t *testing.T) {
+	tests := []struct {
+		name    string
+		infile  string
+		outfile string
+	}{
+		{
+			name:    "lorem",
+			infile:  "testdata/lorem.txt",
+			outfile: "testdata/lorem.lzss",
+		},
+		{
+			name:    "short",
+			infile:  "testdata/short.txt",
+			outfile: "testdata/short.lzss",
+		},
+		{
+			name:    "blink",
+			infile:  "testdata/blink.bin",
+			outfile: "testdata/blink.lzss",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			input, err := ioutil.ReadFile(tt.infile)
+			if err != nil {
+				t.Fatal("couldn't open test file")
+			}
+
+			want, err := ioutil.ReadFile(tt.outfile)
+			if err != nil {
+				t.Fatal("couldn't open test file")
+			}
+
+			got := Encode(input)
+			if !bytes.Equal(want, got) {
+				t.Error("encoding failed")
+			}
+		})
+	}
 }
