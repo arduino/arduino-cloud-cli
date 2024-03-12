d62d97293c00d4eb06380b1b2c9e60f63237fb26
Marco Colombo <ing.marco.colombo@gmail.com>
2024-03-12 15:01:22 +0100

    Added support for UNO R4

--- command/ota/generate.go
+++ command/ota/generate.go
@@ -41,6 +41,7 @@ var (
 		"arduino:mbed_opta:opta":              "0064",
 		"arduino:mbed_giga:giga":              "0266",
 		"arduino:esp32:nano_nora":             "0070",
+		"arduino:renesas_uno:unor4wifi":       "1002",
 	}
 	esp32MagicNumberPart1 = "4553"
 	esp32MagicNumberPart2 = "5033"
